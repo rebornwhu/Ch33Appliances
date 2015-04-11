9b75837df35db4f48ad64ac13d134c85b5543ca2
rebornwhu <rebornwhu@gmail.com>
2015-04-11 15:58:58 -0700

    first commit

---
+++ Ch33Appliances/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ch33Appliances
+//
+//  Created by Xiao Lu on 4/11/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
